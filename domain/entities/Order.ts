9637d55d3b96bef10827fea2a9bc5d73cdd9e97d
shamil-b <shamilbedru47@gmail.com>
2025-01-03 10:59:56 +0300

    Refactor: Remove Delivery interface and add Order interface for improved data management

---
+++ domain/entities/Order.ts
@@ -0,0 +1,12 @@
+import { Consumer } from "./Consumer";
+import { Parcel } from "./Product";
+
+export interface Order {
+  id: string
+  consumer: Consumer
+  bill_id: string
+  latest_time_of_arrival: Date
+  parcel: Parcel;
+  status: "PENDING" | "PICKED_UP" | "DELIVERED";
+  delivery_job_id: string
+} 
