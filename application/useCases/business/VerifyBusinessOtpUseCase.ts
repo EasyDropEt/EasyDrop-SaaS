86de5dc8f6fefa1a91e2004aa7b203a61a17861f
shamil-b <shamilbedru47@gmail.com>
2025-06-04 12:00:00 +0300

    Refactor VerifyBusinessOtpUseCase to utilize BusinessDto for improved type safety in OTP verification response

--- application/useCases/business/VerifyBusinessOtpUseCase.ts
+++ application/useCases/business/VerifyBusinessOtpUseCase.ts
@@ -1,10 +1,10 @@
-import { Business } from '@/domain/entities/Business';
+import { BusinessDto } from '@/domain/entities/Business';
 import { IBusinessAccountRepository } from '@/domain/repositories/IBusinessAccountRepository';
 
 export class VerifyBusinessOtpUseCase {
   constructor(private businessAccountRepository: IBusinessAccountRepository) {}
 
-  async execute(userId: string, otp: string): Promise<{ token: string; business: Business }> {
+  async execute(userId: string, otp: string): Promise<{ token: string; business: BusinessDto }> {
     return this.businessAccountRepository.verifyOtp(userId, otp);
   }
 } 
