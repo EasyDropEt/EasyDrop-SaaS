9620aae949de589f4bb5d0db7a0a776626343b4d
shamil-b <shamilbedru47@gmail.com>
2025-06-04 11:59:50 +0300

    Feat: Refactor CreateBusinessAccountUseCase and GetBusinessOtpUseCase to utilize DTOs for improved data handling and type safety

--- application/useCases/business/GetBusinessOtpUseCase.ts
+++ application/useCases/business/GetBusinessOtpUseCase.ts
@@ -1,9 +1,10 @@
+import { UnverifiedUserDto } from '@/domain/entities/Business';
 import { IBusinessAccountRepository } from '@/domain/repositories/IBusinessAccountRepository';
 
 export class GetBusinessOtpUseCase {
   constructor(private businessAccountRepository: IBusinessAccountRepository) {}
 
-  async execute(phoneNumber: string, password: string): Promise<{ message: string; user_id: string }> {
+  async execute(phoneNumber: string, password: string): Promise<UnverifiedUserDto> {
     return this.businessAccountRepository.getOtp(phoneNumber, password);
   }
 } 
