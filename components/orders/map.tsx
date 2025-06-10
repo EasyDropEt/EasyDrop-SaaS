bb198cda67788e54d797b1aa8fb6525240bf2348
shamil-b <shamilbedru47@gmail.com>
2025-06-10 12:18:10 +0300

    Fix: remove error message heading in MapComponent

--- components/orders/map.tsx
+++ components/orders/map.tsx
@@ -59,7 +59,6 @@ const MapComponent = ({ orderId }: MapComponentProps) => {
                         <svg className="w-12 h-12 text-red-500 dark:text-red-400 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
-                        <h3 className="text-lg font-medium text-dark-900 dark:text-white mb-2">Error Loading Map</h3>
                         <p className="text-dark-500 dark:text-light-400 mb-4">{error}</p>
                         
                         {isAuthError && (
